0d2245c5c8b5cecf5f1d0eba74e88f9780ac58dc
Valentin Stjepic-Cosic <valentin.stjepiccosic.2012@gmail.com>
2019-01-26 15:24:46 +0100

    Implement login

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
 const MongoClient = require('mongodb').MongoClient
 const ObjectID = require('mongodb').ObjectID
 
+const jwt_secret = '12345678' // :)
+
 const app = express()
 
+app.use(bodyParser.json())
 app.use(express.static('public'))
 
 var db
@@ -15,6 +22,21 @@ MongoClient.connect('mongodb://localhost:27017', (err, dbConnection) => {
         db = dbConnection.db('webshop')
     }
 })
+
+app.post('/api/user/login', (req, res) => {
+    db.collection('users').findOne({ email: req.body.email }, (err, result) => {
+        if (err) {
+            res.sendStatus(500)
+        } else if (bcrypt.compareSync(req.body.password, result.password)) {
+            delete result.password
+            const token = jwt.sign(result, jwt_secret, { expiresIn: 86400 })
+            res.json({ token: token })
+        } else {
+            res.sendStatus(403)
+        }
+    })
+})
+
 
 app.get('/api/categories', (req, res) => {
     db.collection('categories').find().toArray((err, data) => {
