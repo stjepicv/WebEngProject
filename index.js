87fc56efd8e34de50ab44f366aedc4be74003a01
Valentin Stjepic-Cosic <valentin.stjepiccosic.2012@gmail.com>
2019-01-26 12:54:01 +0100

    Add list of categories

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ const app = express()
 app.use(express.static('public'))
 
 var db
-MongoClient.connect('mongodb://localhost:27017/webshop', (err, database) => {
+MongoClient.connect('mongodb://localhost:27017', (err, dbConnection) => {
     if (err) {
         console.log('Error connecting to database')
     } else {
         console.log('Connected to MongoDB')
-        db = database
+        db = dbConnection.db('webshop')
     }
 })
 
-
+app.get('/api/categories', (req, res) => {
+    db.collection('categories').find().toArray((err, data) => {
+        if (err) {
+            res.sendStatus(500)
+        } else {
+            res.json(data)
+        }
+    })
+})
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
